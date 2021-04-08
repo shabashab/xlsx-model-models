43d7275c3eb3f6afde7b79e37d8f375dcd720fcb
shabashab <shabashab@ukr.net>
2021-04-08 13:27:01 +0300

    Add level to column markup and collapsed boolean to both row markup and column markup

--- src/Markup/ColumnMarkup.ts
+++ src/Markup/ColumnMarkup.ts
@@ -1,7 +1,11 @@
 export default class ColumnMarkup {
   public width: number;
+  public level: number;
+  public collapsed: boolean;
 
-  constructor(width: number) {
+  constructor(width: number, level: number, collapsed?: boolean) {
     this.width = width;
+    this.level = level;
+    this.collapsed = collapsed || false;
   }
 }
--- src/Markup/RowMarkup.ts
+++ src/Markup/RowMarkup.ts
@@ -1,9 +1,11 @@
 export default class RowMarkup {
   public height: number;
   public level: number;
+  public collapsed: boolean;
 
-  constructor(height: number, level: number) {
+  constructor(height: number, level: number, collapsed?: boolean) {
     this.height = height;
     this.level = level;
+    this.collapsed = collapsed || false;
   }
 }
