aca6e82e2e40b0f234f3c823c35f954dbe39d108
shabashab <shabashab@ukr.net>
2021-04-08 11:59:43 +0300

    Fix exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
-import WorkbookModel from "./WorkbookModel";
-import WorksheetModel from "./WorksheetModel";
-import RowModel from "./RowModel";
-import CellModel from "./CellModel";
-import CellStyle from "./CellStyle";
-import TableMarkup from "./Markup/TableMarkup";
-import RowMarkup from "./Markup/RowMarkup";
-import ColumnMarkup from "./Markup/ColumnMarkup";
-
-export {
-  WorkbookModel,
-  WorksheetModel,
-  RowModel,
-  CellModel,
-  CellStyle,
-  TableMarkup,
-  RowMarkup,
-  ColumnMarkup,
-};
+export { default as WorkbookModel } from "./WorkbookModel";
+export { default as WorksheetModel } from "./WorksheetModel";
+export { default as RowModel } from "./RowModel";
+export { default as CellModel } from "./CellModel";
+export { default as CellStyle } from "./CellStyle";
+export { default as TableMarkup } from "./Markup/TableMarkup";
+export { default as RowMarkup } from "./Markup/RowMarkup";
+export { default as ColumnMarkup } from "./Markup/ColumnMarkup";
