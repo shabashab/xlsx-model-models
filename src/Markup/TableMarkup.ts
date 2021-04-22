bfe18571408a42d1c34742701e80b1b8b14d95b1
shabashab <shabashab@ukr.net>
2021-04-22 14:27:41 +0300

    Add rows count and columns count to table markup

--- src/Markup/TableMarkup.ts
+++ src/Markup/TableMarkup.ts
@@ -6,12 +6,19 @@ import { Dictionary } from "typescript-collections";
 export default class TableMarkup {
   public rows: Dictionary<number, RowMarkup>;
   public columns: Dictionary<number, ColumnMarkup>;
+  public rowsCount: number;
+  public columnsCount: number;
 
   constructor(
     rows?: Dictionary<number, RowMarkup>,
     columns?: Dictionary<number, ColumnMarkup>,
+    rowsCount?: number,
+    columnsCount?: number,
   ) {
     this.rows = rows || new Dictionary<number, RowMarkup>();
     this.columns = columns || new Dictionary<number, ColumnMarkup>();
+
+    this.rowsCount = rowsCount || 0;
+    this.columnsCount = columnsCount || 0;
   }
 }
